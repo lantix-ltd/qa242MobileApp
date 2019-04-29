a9f44feaa531826b9be763d3368c966c555211fb
Jahanzaib Ramzan <jahanzaibramzan@gmail.com>
2019-04-29 16:01:09 +0500

    changes for android

--- android/app/src/main/java/com/qaproject/MainApplication.java
+++ android/app/src/main/java/com/qaproject/MainApplication.java
@@ -3,7 +3,6 @@ package com.qaproject;
 import android.support.multidex.MultiDexApplication;
 
 import com.facebook.react.ReactApplication;
-import com.azendoo.reactnativesnackbar.SnackbarPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.github.yamill.orientation.OrientationPackage;
@@ -40,7 +39,6 @@ public class MainApplication extends MultiDexApplication implements ReactApplica
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
             new MainReactPackage(),
-            new SnackbarPackage(),
             new ReactVideoPackage(),
             new LinearGradientPackage(),
             new OrientationPackage(),
